216706eb8de63ebd3df7e16b1e814f8552b182c2
Marten Postma <postmam@corderius.nl>
2025-10-29 20:39:46 +0100

    github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,10 +22,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://${organizationName}.github.io',
+  url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: `/${projectName}/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
